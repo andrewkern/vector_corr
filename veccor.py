f05f51d875086b5ffd764da085b7732b5935ec08
andrewkern <adkern@uoregon.edu>
2020-07-31 08:51:39 -0700

    rewriting stuff

--- veccor.py
+++ veccor.py
@@ -10,33 +10,41 @@ def veccor(u1, v1, u2, v2):
     u is angle? v is magnitude?
     """
     
+    # not sure we need to do this centering
     u1 -= np.mean(u1)
     v1 -= np.mean(v1)
     u2 -= np.mean(u2)
     v2 -= np.mean(v2)
+    # stuff vectors into matrix
     x = np.array([u1,v1,u2,v2])
+    # compute covariance matrix
     sigma = np.cov(x)
+    # f terms are those that appear from left to right in Crosby et al
+    # need to double check these-- likely bug in indexes called
     f1 = sigma[0,0] * ((sigma[2,2] * sigma[1,3]**2) + (sigma[3,3] * sigma[1,2]**2))
     f2 = sigma[1,1] * ((sigma[2,2] * sigma[1,3]**2) + (sigma[3,3] * sigma[1,2]**2))
     f3 = 2 * sigma[0,1] * sigma[0,3] * sigma[1,2] * sigma[2,3]
     f4 = 2 * sigma[0,1] * sigma[0,2] * sigma[1,3] * sigma[2,3]
-    f6 = -2 * sigma[0,0] * sigma[1,2] * sigma[1,3] * sigma[2,3]
-    f7 = -2 * sigma[1,1] * sigma[0,2] * sigma[0,3] * sigma[2,3]
-    f8 = -2 * sigma[2,2] * sigma[0,1] * sigma[0,3] * sigma[1,3]
-    f9 = -2 * sigma[3,3] * sigma[0,1] * sigma[0,2] * sigma[1,2]
-
+    f5 = -2 * sigma[0,0] * sigma[1,2] * sigma[1,3] * sigma[2,3]
+    f6 = -2 * sigma[1,1] * sigma[0,2] * sigma[0,3] * sigma[2,3]
+    f7 = -2 * sigma[2,2] * sigma[0,1] * sigma[0,3] * sigma[1,3]
+    f8 = -2 * sigma[3,3] * sigma[0,1] * sigma[0,2] * sigma[1,2]
+    # terms in denominator
     g1 = sigma[0,0] * sigma[1,1] - sigma[0,1]**2
     g2 = sigma[2,2] * sigma[3,3] - sigma[2,3]**2
+
+    # debug me
     print(x)
     print(sigma)
-    print(f1,f2,f3,f4,f6,f7,f8,f9)
+    print(f1,f2,f3,f4,f5,f6,f7,f8)
     print(g1,g2)
-    r = (f1 + f2 + f3 + f4 +  f6 + f7 + f8 + f9) / (g1 * g2)
+    r = (f1 + f2 + f3 + f4 +  f5 + f6 + f7 + f8) / (g1 * g2)
     return r
 
-
+# test array set up
 u1 = np.array([1., 2., 3.])
 v1 = np.array([10.,1.,5.])
+# pefect correlation?
 u2 = u1 
 v2 = v1 
 
